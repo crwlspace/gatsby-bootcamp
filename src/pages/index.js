ab0f2c5c6fab7693111f94a716692a31877f593a
Christian Reuterwall <christian.reuterwall@gmail.com>
2020-06-23 13:39:53 +0200

    index.js clean up

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,6 @@ const IndexPage = () => {
       <Head title="Home"></Head>
       <h1>Hello.</h1>
       <h2>I am Christian, a DevOps living and learning new stuff in Berlin.</h2>
-      <p>Need a developer? <Link to="/contact">Contact me.</Link></p>
     </Layout>
   )
 }
